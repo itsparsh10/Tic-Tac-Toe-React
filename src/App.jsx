ff69c3bf99f43da996cc8a67fd791a065b317265
Sparsh Sharma <142775479+itsparsh10@users.noreply.github.com>
2024-06-16 23:54:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
   return (
+    
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
